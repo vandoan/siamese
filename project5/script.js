27eb3495d28a174a74068abc212a5a54328adc90
Van Doan <vdoan09@gmail.com>
2014-10-09 20:17:51 -0400

    page 275

--- project5/script.js
+++ project5/script.js
@@ -1,36 +1,8 @@
- // even code from the book is not working
-
-
-function checkLength(e, minLength) {         // Declare function
-  if (!e) {                                  // If event object doesn't exist
-    e = window.event;                        // Use IE fallback
- }
- return e.target || e.srcElement; 
+function setup() {
+  var textInput; 
+  textInput = document.getElementById('username'); 
+  textInput.focus(); 
 }
 
-function itemDone(e) {
-  var target, elParent, elGrandparent; 
-  target = getTarget(e); 
-  elParent = target.parentNode; 
-  elGrandparent = target.parentNode.parentNode; 
-  elGrandparent.removeChild(elParent);
-
-  // prevent the link from taking you elsewhere
-  if (e.preventDefault) { 
-    e.preventDefault(); 
-  } else {
-    e.returnValue = false; 
-  } 
-}
-
-// set up event listeners to call itemDone() on click 
-var el = document.getElementById('shoppingList'); 
-if (el.addEventListener) {
-  el.addEventListender('click', function(e_ {
-    itemDone(e); 
-  }, false); 
-} else {
-  el.attachEvent('onclick', function(e){
-    itemDone(e); 
-  }); 
-}
+window.addEventListener('load', setup, false); 
+  // when page loads, call setup ()
