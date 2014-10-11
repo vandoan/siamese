dbed81877abbed7c4a213155b5bce0c6e19a5586
Van Doan <vdoan09@gmail.com>
2014-10-11 00:46:10 -0400

    321

--- project9/script.js
+++ project9/script.js
@@ -1,2 +1,6 @@
-var $listItemHTML = $('li').html(); 
-$('li').append('<i>' + $listItemHTML + '</i>');
+$(function() {
+	$('ul').before('<p class= "notice">just updated</p>'); 
+	$('li.hot').prepend('+ '); 
+	var $newListItem = $('<li><em>gluten-free</em> soysauce</li'); 
+	$('li:last').after($newListItem); 
+}); 
