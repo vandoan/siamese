aa02800e14e7499b34c071e10d84ebaaaaf9e819
Van Doan <vdoan09@gmail.com>
2014-10-13 01:36:02 -0400

    365

--- project22/script.js
+++ project22/script.js
@@ -1,15 +1,54 @@
-$(function() {
-	var $window = $(window); 
-	var $slideAd = $('#slideAd');
-	var endZone = $('#footer').offset().top - $window.height() - 500;
+ $(function() { 
 
-		$window.on('scroll', function() {
+ 	var $list, $newItemForm, $newItemButton; 
+ 	var item = ''; 
+ 	$list = $('ul'); 
+ 	$newItemForm = $('#newItemForm'); 
+ 	$newItemButton = $('#newItemButton'); 
 
-			if ( (endzone) < $window.scrollTop() ) {
-				$slideAd.animate({ 'right': '0px'}, 250); 
-			} else { 
-				$slideAd.stop(true).animate({ 'right': '-360px'}, 250); 
-			} 
+ 	$('li').hide().each(function(index) { 
+ 		$(this).delay(450 * index).fadeIn(1600); 
+ 	}); 
 
-	}); 
+ 	function updateCount() { 
+ 		var items = $('li[class!=complete]').length; 
+ 		$('#counter').text(items); 
+
+ }
+updateCount(); 
+$newItemButton.show(); 
+$newItemForm.hide(); 
+$('#showForm').on('click',function(){ 
+	$newItemButton.hide(); 
+	$newItemForm.show(); 
 });
+
+$newItemForm.on('submit', function(e) {
+	e.preventDefault(); 
+	var text = $('input:text').val(); 
+	$list.append('<li>' + text + '</li>'); 
+	$('input:text').val(''); 
+	updateCount(); 
+}); 
+
+$list.on('click', 'li', function() {
+	var $this = $(this);
+	var complete = $this.hasClass('complete'); 
+
+if (complete === true){
+	$this.animate({
+		opacity: 0.0, 
+		paddingLeft: '+=180' 
+	}, 500, 'swing', function() { 
+		$this.remove(); 
+	}); 
+} else { 
+	item = $this.text(); 
+	$this.remove(); 
+	$list 
+		.append('<li class=\"complete\">' + item + '</li>')
+			.hide().fadeIn(300); 
+			updateCount(); 
+		} 
+	}); 
+});    
