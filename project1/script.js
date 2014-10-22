1b205128d2f3410fbe253a66d3ee726c86f96e62
Van Doan <vdoan09@gmail.com>
2014-10-21 22:16:41 -0400

    387

--- project1/script.js
+++ project1/script.js
@@ -1,13 +1,33 @@
-var xhr = new XMLHttpRequest(); 
+function showEvents(data) {
+	var newContent = ''; 
 
-xhr.onload = function() {
-
-	if (xhr.status === 200) {
-		document.getElementById('content').innerHTML = xhr.repsonseText; 
+	for (var i = 0; i < data.events.length; i++) {
+		newContent += '<div class="event">'; 
+		newContent += '<img src="' + data.events[i].map + '"'; 
+		newContent += ' alt="' + data.events[i].location + data.events[i].location + '" />'; 
+		newContent += '<p><b>' + data.events[i].location + '</b><br>'; 
+		newContent += data.events[i].data + '</p>'; 
+		newContent += '</div>'; 
 	}
-}; 
+		
+document.getElementById('content').innerHTML = newContent; }
 
-xhr.open('GET', '../data/data.html', true); 
-xhr.send(null); 
-	}
-}
+	showEvents({
+		"events": [ 
+		{
+			"location": "San Francisco, CA", 
+			"data": "May 1", 
+			"map": "img/map-ca.png"
+		},
+		{
+			"location": "Austin, TX", 
+			"date": "May 15", 
+			"map": "img/map-tx.png"
+		}, 
+		{	
+			"location": "New York, NY", 
+			"data": "May 30", 
+			"map": "img/map-ny.png"
+		}
+	]
+});
